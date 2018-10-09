c7d3b2d8257fc4858d58895defc3f4fa19286e23
Michael Naples <37196117+MNaplesDevelopment@users.noreply.github.com>
2018-10-09 17:02:15 -0400

    Update testing.py

--- src/utils/code_etl/testing.py
+++ src/utils/code_etl/testing.py
@@ -2,10 +2,12 @@ import re
 from pyspark import SparkContext, SparkConf
 from pyspark.sql import SQLContext
 from pyspark.mllib.feature import Word2Vec
+import _pickle as pickle
+import json
+import numpy as np
 
 sc = SparkContext()
 sqlCtx = SQLContext(sc)
-print('hi')
 code_lines = sqlCtx.read.option("multiLine", True).option("mode", "PERMISSIVE").json("\jsons\hi.json")
 code_lines = code_lines.repartition(300)
 
@@ -22,10 +24,18 @@ words = code_lines\
     .map(lambda line: split_code(line))\
     .filter(lambda line: line != [])
 
-print(words)
-
 word2vec = Word2Vec()
-word2vec.setMinCount(25)    # Default 100
+word2vec.setMinCount(2)    # Must be atleast 2 occurrences of a word to produce a vector
 word2vec.setVectorSize(50)  # Default 100
 model = word2vec.fit(words)
 
+model_dict = {k: list(v) for k, v in dict(model.getVectors()).items()}
+
+with open("py2vec_model.json", "w") as f:
+    json.dump(model_dict, f, indent=4)
+
+model_dict = {k: np.array(list(v)) for k, v in dict(model.getVectors()).items()}
+
+with open("py2vec_model.pkl", "wb") as f:
+    pickle.dump(model_dict, f)
+    
