8078f3096cca35dbbf12565d65fa433df8dd9bb5
Michael Naples <37196117+MNaplesDevelopment@users.noreply.github.com>
2018-10-13 13:10:29 -0400

    Update repo_to_json.py

--- src/repo_to_json.py
+++ src/repo_to_json.py
@@ -95,7 +95,7 @@ def get_filelist(directory):
     file_list = []
     for root, _, files in os.walk(directory):
         for file in files:
-            if file.endswith('.java'):
+            if file.endswith('.py'):
                 file_list.append(root + '/' + file)
 
     return file_list
